4419b1aeffebe459750b4a82775ab556f0eb6d78
Leandro Fogaça <fogaca.lf@gmail.com>
2022-08-05 19:32:19 -0300

    Update renderer.js
    add function to connect/open the port

--- src/renderer.js
+++ src/renderer.js
@@ -1,5 +1,7 @@
 
 const { SerialPort } = require('serialport')
+// eslint-disable-next-line no-unused-vars
+const { ReadlineParser } = require('@serialport/parser-readline')
 
   
  
@@ -17,11 +19,33 @@ const listSerialPorts = async function () {
 }
   
 
-const connect = async function (e) {
+const connect = async function (port, baudRate ) {
   
-  console.log("conectar na porta: ", e)
+  console.log("conectar na porta: ", port, " e ", baudRate)
 
+
+
+  const Port = new SerialPort({
+    path: port,
+    baudRate: baudRate,
+    autoOpen: false,
+  })
+  
+  Port.open((err) => {if (err) {
+    console.log('Error opening port: ', err.message)
+  }
+})
+
+// The open event is always emitted
+Port.on('open', function() {
+  console.log('PORTA FOI ABERTA');
+})
+  
+  return Port
+   
+  
 }
+
 
 
 
